6832e9308ae9022cc515a1e5859a938c47283e2b
ranapoojar <pooja.rana@tntra.io>
2023-03-30 17:10:52 +0530

    [added]oops concept

---
+++ oops/Encapsulations/private_accessmodifier.py
@@ -0,0 +1,58 @@
+class Employee:
+    # constructor
+    def __init__(self, name, salary):
+        # public data member
+        self.name = name
+        # private member
+        self.__salary = salary
+
+# creating object of a class
+emp = Employee('Jessa', 10000)
+
+#OUTPUT :-Show error like AttributeError: 'Employee' object has no attribute '__salary'
+
+# accessing private data members
+print('Salary:', emp.__salary)
+
+
+
+#Create public method to access private members
+
+class Employee:
+    # constructor
+    def __init__(self, name, salary):
+        # public data member
+        self.name = name
+        # private member
+        self.__salary = salary
+
+    # public instance methods
+    def show(self):
+        # private members are accessible from a class
+        print("Name: ", self.name, 'Salary:', self.__salary)
+
+# creating object of a class
+emp = Employee('Jessa', 10000)
+
+# calling public method of the class
+emp.show()
+
+
+
+# Name Mangling to access private members
+
+class Employee:
+    # constructor
+    def __init__(self, name, salary):
+        # public data member
+        self.name = name
+        # private member
+        self.__salary = salary
+
+# creating object of a class
+emp = Employee('Jessa', 10000)
+
+print('Name:', emp.name)
+# direct access to private member using name mangling
+print('Salary:', emp._Employee__salary)
+
